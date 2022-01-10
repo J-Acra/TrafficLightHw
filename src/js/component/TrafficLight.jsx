c48641e06b77a915ac9b8737f3091d5629159c23
Jonathan Acra <jonathanacra@gmail.com>
2022-01-10 18:52:06 +0000

    added change light button instead of clicking individual divs

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,26 +1,46 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const TrafficLight = () => {
 	const [blight, setBright] = useState("");
-	const lightArray = [0, 1, 2];
+	const [count, setCount] = useState(0);
+	const lightArray = ["brightRed", "brightYellow", "brightGreen"];
+	// useEffect(() => {
+	// 	const interval = setInterval(() => {
+	// 		changeLight();
+	// 	}, 1000);
+	// 	return () => clearInterval(interval);
+	// }, []);
+	function changeLight() {
+		setBright(lightArray[count]);
+		if (count < lightArray.length) {
+			if (count == lightArray.length - 1) {
+				setCount(0);
+			} else {
+				setCount(count + 1);
+			}
+		} else {
+			setCount(0);
+		}
+		console.log("count is " + count);
+	}
+
 	return (
 		<div className="bigDiv">
-			<button className="changeLight">Change</button>
+			<button onClick={() => changeLight()} className="changeLight">
+				Change
+			</button>
 			<div className="stem"></div>
 			<div className="lightHousing">
 				<div
-					onClick={() => setBright("brightRed")}
 					className={`red light ${
 						blight == "brightRed" && "redBright"
 					}`}></div>
 				<div
-					onClick={() => setBright("brightYellow")}
 					className={`yellow light ${
 						blight == "brightYellow" && "yellowBright"
 					}`}></div>
 				<div
-					onClick={() => setBright("brightGreen")}
 					className={`green light ${
 						blight == "brightGreen" && "greenBright"
 					}`}></div>
